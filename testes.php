28653bb4cf8823faf4eaa8792610be4c3631c4e8
Miguel <miguel_santos96@hotmail.com>
2018-01-10 17:42:20 +0000

    Testing

--- testes.php
+++ testes.php
@@ -1,4 +1,7 @@
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" >
-  Launch demo modal
-</button>
+
+<div class="btn-group" style="width:70%;height:100%; ">
+<a href="#" style="width:50%;background-image:url(Images/buttonbg.png)">Abrir File</a>
+<a href="#" style="width:50%;background-image:url(Images/buttonbg.png)">Open File</a>
+<a href="#" style="width:50%;background-image:url(Images/buttonbg.png)">Delete File</a>
+<a href="#" style="width:50%;background-image:url(Images/buttonbg.png)">Edit File</a>
+</div>
